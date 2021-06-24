54e76927d30ea7864a6e05895902dec98dc41850
Prashant Kumar <prashantbittu123@gmail.com>
2021-06-24 14:07:07 +0530

    new way to transcode

--- main.go
+++ main.go
@@ -70,6 +70,8 @@ func main() {
 	// Transcode("sample.mp4", "854", "480")
 	// Transcode("sample.mp4", "1280", "720")
 	// MakeMasterFile("master_file")
-	TranscodeAIO("pass.mkv")
+
+	// TranscodeAIO("pass.mkv")
+	MakeMasterFile("master_file")
 
 }
--- transcode.go
+++ transcode.go
@@ -31,16 +31,24 @@ func Transcode(uuid string, w string, h string) {
 }
 
 func MakeMasterFile(fileName string) {
-	str := `#EXTM3U
-#EXT-X-STREAM-INF:BANDWIDTH=375000,RESOLUTION=640x360
-360_out.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480
-480_out.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720
-480_out.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=3500000,RESOLUTION=1920x1080
-480_out.m3u8`
+	// 	str := `#EXTM3U
+	// #EXT-X-STREAM-INF:BANDWIDTH=375000,RESOLUTION=640x360
+	// 360_out.m3u8
+	// #EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480
+	// 480_out.m3u8
+	// #EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720
+	// 480_out.m3u8
+	// #EXT-X-STREAM-INF:BANDWIDTH=3500000,RESOLUTION=1920x1080
+	// 480_out.m3u8`
 
+	str := `#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360
+360p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480
+480p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720
+720p.m3u8`
 	outfile, err := os.Create("./temp/sample/hls/" + fileName + ".m3u8")
 	if err != nil {
 		panic(err)
@@ -53,21 +61,30 @@ func MakeMasterFile(fileName string) {
 
 func TranscodeAIO(fileName string) {
 	// 640x360 , 854x480 , 1280x720
+
 	cmd := exec.Command("ffmpeg", "-y", "-i", "./temp/"+fileName,
-		"-preset", "medium", "-g", "48", "-sc_threshold", "0",
-		"-map", "0:0", "-map", "0:1", "-map", "0:0", "-map", "0:1",
-		"-s:v:0", "640x360", "-c:v:0", "libx264", "-b:v:0", "365k",
-		"-s:v:1", "960x540", "-c:v:1", "libx264", "-b:v:1", "2000k",
-		"-c:a", "copy",
-		"-var_stream_map", "v:0,a:0 v:1,a:1",
-		"-master_pl_name", "master.m3u8",
-		"-f", "hls",
-		"-hls_time", "6",
-		"-hls_playlist_type", "vod",
-		"-hls_list_size", "0",
-		"-hls_segment_filename", "./temp/sample/hls/v%v/fileSequence%d.ts",
-		"./temp/sample/hls/v%v/prog_index.m3u8",
-	)
+		"-vf", "scale=w=640:h=360:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "20", "-sc_threshold", "0", "-g", "48", "-keyint_min", "48", "-hls_time", "4", "-hls_playlist_type", "vod", "-b:v", "800k", "-maxrate", "856k", "-bufsize", "1200k", "-b:a", "96k", "-hls_segment_filename", "./temp/sample/hls/360p_%03d.ts", "./temp/sample/hls/360p.m3u8",
+		"-vf", "scale=w=842:h=480:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "20", "-sc_threshold", "0", "-g", "48", "-keyint_min", "48", "-hls_time", "4", "-hls_playlist_type", "vod", "-b:v", "1400k", "-maxrate", "1498k", "-bufsize", "2100k", "-b:a", "128k", "-hls_segment_filename", "./temp/sample/hls/480p_%03d.ts", "./temp/sample/hls/480p.m3u8",
+		"-vf", "scale=w=1280:h=720:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "20", "-sc_threshold", "0", "-g", "48", "-keyint_min", "48", "-hls_time", "4", "-hls_playlist_type", "vod", "-b:v", "2800k", "-maxrate", "2996k", "-bufsize", "4200k", "-b:a", "128k", "-hls_segment_filename", "./temp/sample/hls/720p_%03d.ts", "./temp/sample/hls/720p.m3u8")
+	//"-vf", "scale=w=1920:h=1080:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "20", "-sc_threshold", "0", "-g", "48", "-keyint_min", "48", "-hls_time", "4", "-hls_playlist_type", "vod", "-b:v", "5000k", "-maxrate", "5350k", "-bufsize", "7500k", "-b:a", "192k", "-hls_segment_filename", "./temp/sample/hls/1080p_%03d.ts", "./temp/sample/hls/1080p.m3u8")
+
+	// cmd := exec.Command("ffmpeg", "-y", "-i", "./temp/"+fileName,
+	// 	"-preset", "medium", "-g", "48", "-sc_threshold", "0",
+	// 	"-map", "0:0", "-map", "0:1", "-map", "0:0", "-map", "0:1", "-map", "0:0", "-map", "0:1",
+	// 	"-s:v:0", "640x360", "-c:v:0", "libx264", "-b:v:0", "365k",
+	// 	"-s:v:1", "960x540", "-c:v:1", "libx264", "-b:v:1", "2000k",
+	// 	"-s:v:2", "1280x720", "-c:v:2", "libx264", "-b:v:2", "3000k",
+
+	// 	"-c:a", "copy",
+	// 	"-var_stream_map", "v:0,a:0 v:1,a:1,v:2,a:2",
+	// 	"-master_pl_name", "master.m3u8",
+	// 	"-f", "hls",
+	// 	"-hls_time", "6",
+	// 	"-hls_playlist_type", "vod",
+	// 	"-hls_list_size", "0",
+	// 	"-hls_segment_filename", "./temp/sample/hls/v%v/fileSequence%d.ts",
+	// 	"./temp/sample/hls/v%v/prog_index.m3u8",
+	// )
 
 	// cmd := exec.Command("ffmpeg", "-i", "./temp/"+fileName,
 	// 	"-filter_complex",
