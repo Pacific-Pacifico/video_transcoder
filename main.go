7d66b0939778054026eb3d6f69a438e67b134efc
prashant kumar <pacific@codewire>
2021-07-26 11:58:14 +0530

    cleanup

--- main.go
+++ main.go
@@ -2,79 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"runtime"
 	"time"
 )
 
-func GetMediaInfo(res string, fileName string) {
-	if runtime.GOOS == "windows" {
-		fmt.Println("can't run on windows")
-	}
-	var width, height string
-	if res == "240" {
-		width = "352"
-		height = "240"
-	} else if res == "360" {
-		width = "480"
-		height = "360"
-	} else if res == "480" {
-		width = "854"
-		height = "480"
-	} else if res == "720" {
-		width = "1280"
-		height = "720"
-	}
-	// out, err := exec.Command("ls", "-al").Output()
-
-	// cmd := exec.Command("ffprobe", "-v", "quiet", "-print_format", "json", "-show_format",
-	// 	"-show_streams", "./samples/sample.mp4")
-
-	cmd := exec.Command("ffmpeg", "-i", "./samples/"+fileName, "-vf", "scale="+width+":"+height, "./samples/"+res+"_"+fileName)
-
-	// var stdout bytes.Buffer
-	// var stderr bytes.Buffer
-	outfile, err := os.Create("./samples/output+" + res + "_" + fileName + ".txt")
-	if err != nil {
-		panic(err)
-	}
-	defer outfile.Close()
-
-	errfile, err := os.Create("./samples/err_output" + res + ".txt")
-	if err != nil {
-		panic(err)
-	}
-	defer errfile.Close()
-	cmd.Stdout = outfile
-	cmd.Stderr = errfile
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = &stderr
-	err = cmd.Run()
-	fmt.Println("Executing...")
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	fmt.Println("Command executed successfully")
-	// outstr := stdout.String()
-	// errstr := stderr.String()
-	// fmt.Printf("Output : %s", outstr)
-	// fmt.Printf("\nError: %s", errstr)
-}
-
 func main() {
 	start := time.Now()
-	// fmt.Println("Hello Wsl")
-	//GetMediaInfo("480", "sample.mkv")
 	MakeDir("sample")
-	// Transcode("sample.mp4", "640", "360")
-	// Transcode("sample.mp4", "854", "480")
-	// Transcode("sample.mp4", "1280", "720")
-	// MakeMasterFile("master_file")
-
 	TranscodeAIO("sample.mp4")
-	MakeMasterFile("master_file")
 	fmt.Println(time.Since(start))
-
 }
--- transcode.go
+++ transcode.go
@@ -13,34 +13,7 @@ func MakeDir(uuid string) {
 	os.MkdirAll(filepath.Join("temp", uuid, "hls"), 0700)
 }
 
-func Transcode(uuid string, w string, h string) {
-	// 640x360 , 854x480 , 1280x720
-	cmd := exec.Command("ffmpeg", "-i", "./temp/"+uuid,
-		"-profile:v", "baseline",
-		"-level", "3.0", "-s", w+"x"+h,
-		"-start_number", "0",
-		"-hls_time", "10",
-		"-hls_list_size", "0", "-f", "hls",
-		"./temp/sample/hls/"+h+"_out.m3u8",
-	)
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("in transcode cmd.Run() failed with %s\n", err)
-	}
-	fmt.Println("Ran successfully")
-}
-
 func MakeMasterFile(fileName string) {
-	// 	str := `#EXTM3U
-	// #EXT-X-STREAM-INF:BANDWIDTH=375000,RESOLUTION=640x360
-	// 360_out.m3u8
-	// #EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480
-	// 480_out.m3u8
-	// #EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720
-	// 480_out.m3u8
-	// #EXT-X-STREAM-INF:BANDWIDTH=3500000,RESOLUTION=1920x1080
-	// 480_out.m3u8`
-
 	str := `#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360
@@ -72,60 +45,24 @@ func TranscodeAIO(fileName string) {
 		"-vf", "scale=w=1280:h=720:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "23", "-sc_threshold", "0", "-g", "60", "-keyint_min", "60", "-hls_time", "10", "-hls_playlist_type", "vod", "-b:v", "2800k", "-maxrate", "2996k", "-bufsize", "4200k", "-b:a", "128k", "-hls_segment_filename", "./temp/sample/hls/720p_%03d.ts", "./temp/sample/hls/720p.m3u8")
 	//"-vf", "scale=w=1920:h=1080:force_original_aspect_ratio=decrease", "-c:a", "aac", "-ar", "48000", "-c:v", "h264", "-profile:v", "main", "-crf", "23", "-sc_threshold", "0", "-g", "48", "-keyint_min", "48", "-hls_time", "10", "-hls_playlist_type", "vod", "-b:v", "5000k", "-maxrate", "5350k", "-bufsize", "7500k", "-b:a", "192k", "-hls_segment_filename", "./temp/sample/hls/1080p_%03d.ts", "./temp/sample/hls/1080p.m3u8")
 
-	// cmd := exec.Command("ffmpeg", "-y", "-i", "./temp/"+fileName,
-	// 	"-preset", "medium", "-g", "48", "-sc_threshold", "0",
-	// 	"-map", "0:0", "-map", "0:1", "-map", "0:0", "-map", "0:1", "-map", "0:0", "-map", "0:1",
-	// 	"-s:v:0", "640x360", "-c:v:0", "libx264", "-b:v:0", "365k",
-	// 	"-s:v:1", "960x540", "-c:v:1", "libx264", "-b:v:1", "2000k",
-	// 	"-s:v:2", "1280x720", "-c:v:2", "libx264", "-b:v:2", "3000k",
+	// outfile, err := os.Create("./temp/sample/output_" + fileName + ".txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer outfile.Close()
 
-	// 	"-c:a", "copy",
-	// 	"-var_stream_map", "v:0,a:0 v:1,a:1,v:2,a:2",
-	// 	"-master_pl_name", "master.m3u8",
-	// 	"-f", "hls",
-	// 	"-hls_time", "6",
-	// 	"-hls_playlist_type", "vod",
-	// 	"-hls_list_size", "0",
-	// 	"-hls_segment_filename", "./temp/sample/hls/v%v/fileSequence%d.ts",
-	// 	"./temp/sample/hls/v%v/prog_index.m3u8",
-	// )
-
-	// cmd := exec.Command("ffmpeg", "-i", "./temp/"+fileName,
-	// 	"-filter_complex",
-	// 	"[0:v]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=640:h=360[v3out]",
-	// 	"-map", "[v1out]", "-c:v:0", "libx264", "-x264-params", "\"nal-hrd=cbr:force-cfr=1\"", "-b:v:0", "5M", "-maxrate:v:0", "5M", "-minrate:v:0", "5M", "-bufsize:v:0", "10M", "-preset", "slow", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
-	// 	"-map", "[v2out]", "-c:v:1", "libx264", "-x264-params", "\"nal-hrd=cbr:force-cfr=1\"", "-b:v:0", "3M", "-maxrate:v:0", "3M", "-minrate:v:0", "3M", "-bufsize:v:0", "3M", "-preset", "slow", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
-	// 	"-map", "[v3out]", "-c:v:2", "libx264", "-x264-params", "\"nal-hrd=cbr:force-cfr=1\"", "-b:v:0", "1M", "-maxrate:v:0", "1M", "-minrate:v:0", "1M", "-bufsize:v:0", "1M", "-preset", "slow", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
-	// 	"-map", "a:0", "-c:a:0", "aac", "-b:a:0", "96k", "-ac", "2",
-	// 	"-map", "a:0", "-c:a:1", "aac", "-b:a:1", "96k", "-ac", "2",
-	// 	"-map", "a:0", "-c:a:2", "aac", "-b:a:2", "48k", "-ac", "2",
-	// 	"-f", "hls",
-	// 	"-hls_time", "2",
-	// 	"-hls_playlist_type", "vod",
-	// 	"-hls_flags", "independent_segments",
-	// 	"-hls_segment_type", "mpegts",
-	// 	"-hls_segment_filename", "./temp/sample/hls/stream_%v/data%02d.ts",
-	// 	"-master_pl_name", "master.m3u8",
-	// 	"-var_stream_map", "v:0,a:0 v:1,a:1 v:2,a:2", "./temp/sample/hls/stream_%v.m3u8",
-	// )
-
-	outfile, err := os.Create("./temp/sample/output_" + fileName + ".txt")
-	if err != nil {
-		panic(err)
-	}
-	defer outfile.Close()
-
-	errfile, err := os.Create("./temp/sample/err_output" + fileName + ".txt")
+	errfile, err := os.Create("./temp/sample/" + fileName + "_err_output.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer errfile.Close()
-	cmd.Stdout = outfile
+	// cmd.Stdout = outfile
 	cmd.Stderr = errfile
 
 	err = cmd.Run()
 	if err != nil {
 		log.Fatalf("in transcode cmd.Run() failed with %s\n", err)
 	}
+	MakeMasterFile("master_file")
 	fmt.Println("Ran successfully")
 }
